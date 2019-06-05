eacc84306f48d1f3dfb2bb7c6393f1f5ebb4ab67
Peter Severin Rasmussen <git@petersr.com>
2019-06-05 21:05:40 +0200

    added initial display of languages and values

--- backend/langvalues/views.py
+++ backend/langvalues/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+from .models import *
+
+
+def chunks(l, n):
+    """
+    Create len(l)/n chunks of size n from list l.
+    """
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
 def index(request):
+
+    languages = Language.objects.order_by("name")
+    values = Value.objects.order_by("name")
+
+    num_columns = 3
+
+    value_partitions = chunks(values, round(len(values) / num_columns))
+
     context = {
+        "languages": languages,
+        "values": values,
+        "num_columns": num_columns,
+        "value_partitions": value_partitions,
     }
     return render(request, "langvalues/index.html", context)
