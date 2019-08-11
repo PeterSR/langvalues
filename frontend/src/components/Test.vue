5b1fa7f9d9dac32ec745f898b060f86d574fe9c8
Peter Severin Rasmussen <git@petersr.com>
2019-08-11 11:10:27 +0200

    Fronted (now using Vue) loads langs and values from backend and displays them.

---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div v-for="lang in langs">
+            <b>{{lang.name}}</b>
+        </div>
+        <br />
+        <div v-for="value in values">
+            <b>{{value.name}}</b>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    data() {
+        return {
+            langs: [],
+            values: [],
+        }
+    },
+    mounted() {
+        axios.get("/api/langs").then((response) => {
+            this.langs = response.data
+        }).catch((err) => {
+            console.error(err)
+        })
+
+        axios.get("/api/values").then((response) => {
+            this.values = response.data
+        }).catch((err) => {
+            console.error(err)
+        })
+    },
+};
+</script>
+
+<style scoped>
+</style>
